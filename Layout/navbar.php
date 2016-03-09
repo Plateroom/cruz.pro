1a5a53a538b6a026f171708339830abd87b98044
Plateroom <stefania_solari@hotmail.it>
2016-03-10 00:08:34 +0100

    css filippo

--- Layout/navbar.php
+++ Layout/navbar.php
@@ -18,30 +18,87 @@
 
 
 		<!-- Navigation -->
+
 		<nav class="navbar navbar-default navbar-fixed-top">
+		 <div class="container">
+
+
+         <!-- Brand e toggle raggruppati per mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+		 					<a class="navbar-brand" href="#user"><span class="glyphicon glyphicon-asterisk" style="color: #748F80; margin-right: 15px";></span>CRUZ.pro</a>	
+            </div>
+
+
+
+            <!-- link navbar -->
+			
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  	<ul class="nav navbar-nav navbar-right">
+           <li class="hidden">
+          
+           <li>
+              <a class="page-scroll" href="#sale riunioni">Sale riunioni</a>
+           </li>
+           <li>
+              <a class="page-scroll" href="#eventi">Eventi</a>
+           </li>
+           <li>
+             	<a class="page-scroll" href="#aggiungi evento">Aggiungi evento</a>
+           </li>
+           
+						<li class="dropdown">
+						   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profilo
+						    <span class="caret"></span></a>
+							 		<ul class="dropdown-menu">
+								    <li><a href="#">Modifica profilo</a></li>
+								    <li><a href="#">Dashboard</a></li>
+								    <li role="separator" class="divider"></li>
+								    <li><a href="#">Collaboratori</a></li>
+								    <li role="separator" class="divider"></li>
+								    <li><a href="#">Log out</a></li>
+							  	</ul>
+						
+           </li>
+        </ul>
+
+		
+		  </div>
+
+		</nav>
+
+
+
+<!--		<nav class="navbar navbar-default navbar-fixed-top">
 
 			<div class="container">
 				<a class="navbar-brand" href="_user.php"><span class="glyphicon glyphicon-asterisk" style="color: #748F80; margin-right: 15px";></span>CRUZ.pro</a>	
 					
 
-				<ul class="nav nav-pills">
-				  <li role="presentation"><a href="#">elenco sale</a></li>
-				  <li role="presentation" class="active"><a href="#">crea evento</a></li>
-				  <li role="presentation" class="dropdown">
-				  	<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-      				Profilo <span class="caret"></span>
-    				</a>
-				  </li>
-				</ul>	
+		<ul class="nav nav-pills">
+			  <li role="presentation"><a href="#">elenco sale</a></li>
+			  <li role="presentation" class="active"><a href="#">crea evento</a></li>
+			  <li role="presentation" class="dropdown">
+			  	<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+     				Profilo <span class="caret"></span>
+   				</a>
+			  </li>
+			</ul>	
 
 
 
-					<ul class="dropdown-menu">
-						<li><a href="#">modifica</a></li>
-						<li><a href="#">colleghi</a></li>
-						<li role="separator" class="divider"></li>
-						<li><a href="#">log out</a></li>
-					</ul>
+				<ul class="dropdown-menu">
+					<li><a href="#">modifica</a></li>
+					<li><a href="#">colleghi</a></li>
+					<li role="separator" class="divider"></li>
+					<li><a href="#">log out</a></li>
+				</ul>
 
 
 
@@ -51,9 +108,9 @@
 
 		</nav>
 	
-	Contenuto user
+	Contenuto user-->
 
 
-
+ <script src="../assets/js/bootstrap.js"></script>
 	</body>
 </html>

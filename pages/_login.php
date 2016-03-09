7e5dca1a88624a5f39cf0ce7d55426b4a7489b9c
Filippo <sannafilippo.fs@gmail.com>
2016-03-09 19:47:19 +0100

    modifiche login css

--- pages/_login.php
+++ pages/_login.php
@@ -13,10 +13,11 @@
         <link href="../assets/css/stylecruz.css" rel="stylesheet">
 
 	</head>
+
 	 <body>
-	<header>
+		<header>
 		CRUZpro utilizza i cookie per offrirti la migliore esperienza possibile. Ulteriori info Ok!
-	</header>
+		</header>
 
 	 <div class="Allcontenuto">
 	 	<form method="post" action="user.php">
@@ -25,21 +26,14 @@
 		<form name="login" action="index_submit" method="get" accept-charset="utf-8">
    		 <ul>
         	<li><input classe="mailbox" type="email" name="usermail" placeholder="Email Aziendale" required></li><br/>
-        	<li><input type="password" name="password" placeholder= "Password"required></li>
-        	<li><input type="submit" value="Login"></li>
+        	<li><input classe="password" type="password" name="password" placeholder= "Password"required></li>
+        	<li><input class="button" type="submit" value="Login"></li>
     	  </ul>
 		 </form>
 		</section>
 	</div>
 
 	</div>
-
-
-
-
-	
-
-
 
 	</body>
 </html>

b37f4aa268b79d5551b26a170ead002fc56b3f84
Filippo <sannafilippo.fs@gmail.com>
2016-03-10 14:38:09 +0100

    altre modifche login

--- pages/_login.php
+++ pages/_login.php
@@ -13,27 +13,39 @@
         <link href="../assets/css/stylecruz.css" rel="stylesheet">
 
 	</head>
+	
 
-	 <body>
+	<body>
 		<header>
-		CRUZpro utilizza i cookie per offrirti la migliore esperienza possibile. Ulteriori info Ok!
+			CRUZpro utilizza i cookie per offrirti la migliore esperienza possibile. Ulteriori info Ok!
 		</header>
+	
+		 <div class="Allcontenuto">
+	
+		 <img src="Img/Schermatafiltro.jpg">
+				
+		 <div class="form" id="form"> 
+<hr/>
+	   <section class="loginform cf" id="loginform">
+		<form class="login" name="login" action="index_submit" method="get" accept-charset="utf-8">
 
-	 <div class="Allcontenuto">
-	 	<form method="post" action="user.php">
-	 <div class="form" id="form">  
-	   <section class="loginform cf">
-		<form name="login" action="index_submit" method="get" accept-charset="utf-8">
+		
    		 <ul>
-        	<li><input classe="mailbox" type="email" name="usermail" placeholder="Email Aziendale" required></li><br/>
-        	<li><input classe="password" type="password" name="password" placeholder= "Password"required></li>
-        	<li><input class="button" type="submit" value="Login"></li>
+        	<li> <input classe="mailbox" id="mailbox" type="email" name="usermail" placeholder="Email Aziendale" required> </li> <br/>
+        	<li> <input classe="password" id="password" type="password" name="password" placeholder= "Password"required></li>
+        	<li> <input class="button" type="submit" value="Login"> </li>
+
+        	<h6>Creando un account, accetti i Termini di servizio, le Norme sulla privacy e l' utilizzo dei cookie di CRUZpro.</h6>  
+        	
     	  </ul>
-		 </form>
+
+    		
+
+    	 </form>
 		</section>
 	</div>
 
 	</div>
-
-	</body>
-</html>
+	
+</body>
+</html> 

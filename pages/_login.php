fb7da7046c5a607e39b57ad748344f788d372f12
Filippo <sannafilippo.fs@gmail.com>
2016-03-11 12:33:27 +0100

    modifiche login/css

--- pages/_login.php
+++ pages/_login.php
@@ -16,36 +16,40 @@
 	
 
 	<body>
-		<header>
-			CRUZpro utilizza i cookie per offrirti la migliore esperienza possibile. Ulteriori info Ok!
-		</header>
-	
-		 <div class="Allcontenuto">
-	
-		 <img src="Img/Schermatafiltro.jpg">
-				
-		 <div class="form" id="form"> 
-<hr/>
-	   <section class="loginform cf" id="loginform">
-		<form class="login" name="login" action="index_submit" method="get" accept-charset="utf-8">
+	<header>
+		CRUZpro utilizza i cookie per offrirti la migliore esperienza possibile. Ulteriori info Ok!
+	</header>
 
-		
-   		 <ul>
-        	<li> <input classe="mailbox" id="mailbox" type="email" name="usermail" placeholder="Email Aziendale" required> </li> <br/>
-        	<li> <input classe="password" id="password" type="password" name="password" placeholder= "Password"required></li>
+	  <div class="form Allcontenuto"> 
+
+
+    <section class="loginform cf" id="loginform">
+		 <form class="login" name="login" action="index_submit" method="get" accept-charset="utf-8">
+		 
+		 <div class="cruz">
+		 	<p style="font-size: 30px"> CRUZ.pro</p>
+		 	<p style="font-size: 17px">Prenota la tua sala riunioni</p>
+		 	<p style="font-size: 10px">powered by SF</p> 
+		 </div> 
+
+   		  <ul>	
+
+   		  	<hr/>
+
+        	<li> <input class="mailbox" id="mailbox" type="email" name="usermail" placeholder="Email Aziendale" required> </li>
+        	<li> <input class="password" id="password" type="password" name="password" placeholder= "Password"required> </li>
         	<li> <input class="button" type="submit" value="Login"> </li>
+  				
+        		<p id="dimenticata">Password dimenticata?</p>
 
-        	<h6>Creando un account, accetti i Termini di servizio, le Norme sulla privacy e l' utilizzo dei cookie di CRUZpro.</h6>  
-        	
     	  </ul>
-
-    		
-
+    	<div class="termini">
+    	  <p id="riga1">Creando un account, accetti i Termini di servizio, le Norme sulla</p>  
+    	  <p id="riga2">privacy e l'utilizzo dei cookie di CRUZpro.</p>
+    	</div>
     	 </form>
 		</section>
-	</div>
-
-	</div>
+	  </div>
 	
 </body>
 </html> 

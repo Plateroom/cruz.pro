43938ee97f2e04b55b1137227d5cecea68a11a52
Plateroom <stefania_solari@hotmail.it>
2016-03-14 00:31:19 +0100

    sala

--- pages/_sala.php
+++ pages/_sala.php
@@ -1,32 +1,6 @@
 
-	<body>
-<!--second navbar-->
-  <nav class="navbar twoRow" role="navigation">
-    <div class="content-fluid">
-      <div class="col-xs-6 col-xs-offset-4 col-md-4 col-md-offset-4">
-         <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Filtra per</a></li>
-                <li class="divider-vertical"></li>
-                <li class="dropdown">  
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">N. posti<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#">10 posti</a></li>
-                      <li><a href="#">15 posti</a></li>
-                      <li><a href="#">20 posti</a></li>
-                    </ul>
 
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sede<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#">Milano</a></li>
-                      <li><a href="#">Roma</a></li>
-                      <li><a href="#">Napoli</a></li>
-                    </ul>
-                
-          </ul>
-      </div>
-    </div>
-  </nav>
+<!--second navbar-->
 
 
 <!-- contenuto -->
@@ -35,53 +9,120 @@
   
   	<hr>
   
-  	<div class="row">
+  	<div class="row content-img">
   		
         <div class="col-xs-offset-1 col-md-offset-1 col-md-3">
           <div class="panel panel-default">
-            <div class="panel-heading"><h5 class="caratteristiche-titolo">Sala marco Aurelio</h5></div>
-            <div class="panel-body"><h6 class="caratteristiche-titolo">sede La Masa.</h6> <h6 class="text-style">Milano IT</h6>
+            <div class="panel-heading col-xs-offset-2 col-md-offset-2"><h5 class="caratteristiche-titolo text-center" >Sala marco Aurelio</h5></div>
+            <div class="panel-body col-xs-offset-2 col-md-offset-2"><h6 class="caratteristiche-titolo text-center">sede La Masa.</h6> <h6 class="text-style text-center">Milano IT</h6>
             </div>
           </div>
         </div>
         <img src="img/sala.jpg" alt="sala marco aurelio" class="img-circle">
       	<div class="col-md-7">
         	<div class="panel panel-default">
-            <div class="panel-heading"><h5 class="caratteristiche-titolo">Contatti</h5><h6 class="text-style">sala_marcoaurelio@cruzpro.it<br>+393485409834</h5></h6></div>
-            <div class="panel-body"><h5 class="caratteristiche-titolo">20 Posti</h5><h6 class="text-style">
+            <div class="panel-heading"><h5 class="caratteristiche-titolo text-center">Contatti</h5><h6 class="text-style text-center">sala_marcoaurelio@cruzpro.it<br>+393485409834</h5></h6></div>
+            <div class="panel-body"><h5 class="caratteristiche-titolo text-center">20 Posti</h5><h6 class="text-style text-center">
 Internet fibra ottica, WiFi, Proiettore + telo, Lavagna di carta + cancelleria,
 Fotocopiatrice, Frigobar.</h6>
             </div>
           </div>
         </div>
        </div>
-    </div>
+ <!--eventi in ordine -->
 
-  	<div class="row">
-  		<img src="../img/sala.jpg" class="img-circle">
+  	<div class="row content">
+  		
         <div class="col-xs-offset-1 col-md-offset-1 col-md-3">
-          <div class="panel panel-default">
-            <div class="panel-heading"><h5 class="caratteristiche-titolo">Sala marco Aurelio</h5></div>
-            <div class="panel-body"><h6 class="caratteristiche-titolo">sede La Masa.</h6> <h6 class="text-style">Milano IT</h6>
+          <div class="panel panel-success">
+            <div class="panel-heading secondo-contenuto"><h5 class="caratteristiche-titolo text-center">Eventi della sala</h5></div>
+            <div class="panel-body">
+
+
+            	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-warning">
+    <div class="panel-heading" role="tab" id="headingOne">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          I miei eventi
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-warning">
+    <div class="panel-heading" role="tab" id="headingTwo">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          I miei eventi preferiti
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-warning">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          I miei eventi passati
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
             </div>
           </div>
         </div>
       	<div class="col-md-7">
-        	<div class="panel panel-default">
-            <div class="panel-heading"><h5 class="caratteristiche-titolo">Contatti</h5><h6 class="text-style">sala_marcoaurelio@cruzpro.it<br>+393485409834</h5></h6></div>
-            <div class="panel-body"><h5 class="caratteristiche-titolo">20 Posti</h5><h6 class="text-style">
+        	<div class="panel panel-success">
+            <div class="panel-heading">
+
+	             <div id="custom-search-input">
+	               <div class="input-group">
+	                  <input type="text" class="  search-query form-control" placeholder="cerca eventi" />
+	                     <span class="input-group-btn">
+	                      <button class="btn btn-danger" type="button">
+	                     		<span class=" glyphicon glyphicon-search"></span>
+	                      </button>
+	                     </span>
+	               	 </div>
+	                </div>
+	              </div>
+
+
+           <div class="panel-body"><h5 class="caratteristiche-titolo">20 Posti</h5><h6 class="text-style">
 Internet fibra ottica, WiFi, Proiettore + telo, Lavagna di carta + cancelleria,
 Fotocopiatrice, Frigobar.</h6>
             </div>
+          
           </div>
         </div>
        </div>
-    </div>
 
 
 
 </div>
+    
 
-  
+
+
+
+
 	
-	</body>
+

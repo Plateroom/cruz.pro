f8471c6b8cd38f32c6aaae90a59708b75f2b8f51
Filippo <sannafilippo.fs@gmail.com>
2016-04-07 18:07:14 +0200

    Fixed Controller2

--- CRUZ.pro/src/FrontBundle/Controller/UserController.php
+++ CRUZ.pro/src/FrontBundle/Controller/UserController.php
@@ -4,6 +4,6 @@ namespace FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class SalaListaController extends Controller
+class UserController extends Controller
 {
 }

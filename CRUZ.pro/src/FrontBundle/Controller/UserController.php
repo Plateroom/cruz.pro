2e5315316836608203a3b49e72977b1367e18366
Plateroom <stefania_solari@hotmail.it>
2016-04-08 19:16:04 +0200

    user

--- CRUZ.pro/src/FrontBundle/Controller/UserController.php
+++ CRUZ.pro/src/FrontBundle/Controller/UserController.php
@@ -3,7 +3,16 @@
 namespace FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
+   /**
+    * @param Request $request
+    * @return \Symfony\Component\HttpFoundation\Response
+    */
+   public function userAction(Request $request)
+   {
+      return $this->render('FrontBundle:Default:user.html.twig');
+   }
 }

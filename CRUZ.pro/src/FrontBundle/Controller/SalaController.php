5a545cca5360a394a4307249fe4f59bba286f0a2
Plateroom <stefania_solari@hotmail.it>
2016-04-09 17:24:17 +0200

    ultimo

--- CRUZ.pro/src/FrontBundle/Controller/SalaController.php
+++ CRUZ.pro/src/FrontBundle/Controller/SalaController.php
@@ -3,7 +3,17 @@
 namespace FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class SalaController extends Controller
 {
+   /**
+    * @param Request $request
+    * @return \Symfony\Component\HttpFoundation\Response
+    */
+   public function salaAction(Request $request)
+   {
+      return $this->render('FrontBundle:Default:sala.html.twig');
+   }
 }
+

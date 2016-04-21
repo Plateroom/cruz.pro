d24be2c504f18c034a3785f2afa6bfbc402c16fe
Filippo <sannafilippo.fs@gmail.com>
2016-04-21 22:11:01 +0200

    immagine

--- CRUZ.pro/src/FrontBundle/Form/Type/SalaForm.php
+++ CRUZ.pro/src/FrontBundle/Form/Type/SalaForm.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
